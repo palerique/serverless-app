687736a40993be71d43c90595039a511692d0965
Paulo Rodrigues <palerique@gmail.com>
2021-04-04 16:33:31 -0300

    Add initial draft interacting with DynamoDB

--- backend/lambda/getHeroes/src/index.ts
+++ backend/lambda/getHeroes/src/index.ts
@@ -1,8 +1,8 @@
-import {APIGatewayProxyHandler, APIGatewayProxyResult} from 'aws-lambda';
-import {DynamoDB} from 'aws-sdk'
-import {ScanInput} from 'aws-sdk/clients/dynamodb';
+import { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import { DynamoDB } from 'aws-sdk';
+import { ScanInput } from 'aws-sdk/clients/dynamodb';
 
-const dynamoDb = new DynamoDB.DocumentClient()
+const dynamoDb = new DynamoDB.DocumentClient();
 const params: ScanInput = {
   TableName: process.env.DYNAMODB_TABLE || 'Heroes',
 };
@@ -14,22 +14,21 @@ interface Hero {
 
 function getData(): { heroes: Hero[] } {
   const heroes: Hero[] = [
-    {id: '11', name: 'Dr Nice'},
-    {id: '12', name: 'Narco'},
-    {id: '13', name: 'Bombasto'},
-    {id: '14', name: 'Celeritas'},
-    {id: '15', name: 'Magneta'},
-    {id: '16', name: 'RubberMan'},
-    {id: '17', name: 'Dynama'},
-    {id: '18', name: 'Dr IQ'},
-    {id: '19', name: 'Magma'},
-    {id: '20', name: 'Tornado'},
+    { id: '11', name: 'Dr Nice' },
+    { id: '12', name: 'Narco' },
+    { id: '13', name: 'Bombasto' },
+    { id: '14', name: 'Celeritas' },
+    { id: '15', name: 'Magneta' },
+    { id: '16', name: 'RubberMan' },
+    { id: '17', name: 'Dynama' },
+    { id: '18', name: 'Dr IQ' },
+    { id: '19', name: 'Magma' },
+    { id: '20', name: 'Tornado' },
   ];
-  return {heroes};
+  return { heroes };
 }
 
 export const handler: APIGatewayProxyHandler = async (event, context) => {
-
   let result: APIGatewayProxyResult;
 
   // fetch all todos from the database
@@ -40,17 +39,17 @@ export const handler: APIGatewayProxyHandler = async (event, context) => {
       console.error(error);
       result = {
         statusCode: error.statusCode || 501,
-        headers: {'Content-Type': 'text/plain'},
-        body: 'Couldn\'t fetch the todo items.',
+        headers: { 'Content-Type': 'text/plain' },
+        body: "Couldn't fetch the todo items.",
       };
     } else {
       result = {
         statusCode: 200,
-        body: JSON.stringify({result: queryResult, data: getData(), event, context}, null, 2),
+        body: JSON.stringify({ result: queryResult, data: getData(), event, context }, null, 2),
       };
     }
-  })
+  });
 
-  //TODO: fix it
+  // TODO: fix it
   return result;
 };
