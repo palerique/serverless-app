956b2496adeb52d837e7a8663914540d29fee324
Paulo Rodrigues <palerique@gmail.com>
2021-04-04 15:43:36 -0300

    add mocked return from the lambda

--- backend/lambda/getHeroes/src/index.ts
+++ backend/lambda/getHeroes/src/index.ts
@@ -1,8 +1,29 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
+interface Hero {
+  id: string;
+  name: string;
+}
+
+function getData(): { heroes: Hero[] } {
+  const heroes: Hero[] = [
+    { id: '11', name: 'Dr Nice' },
+    { id: '12', name: 'Narco' },
+    { id: '13', name: 'Bombasto' },
+    { id: '14', name: 'Celeritas' },
+    { id: '15', name: 'Magneta' },
+    { id: '16', name: 'RubberMan' },
+    { id: '17', name: 'Dynama' },
+    { id: '18', name: 'Dr IQ' },
+    { id: '19', name: 'Magma' },
+    { id: '20', name: 'Tornado' },
+  ];
+  return { heroes };
+}
+
 export const handler: APIGatewayProxyHandler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ name: 'returned body name!!!!!!', event, context }, null, 2),
+    body: JSON.stringify({ data: getData(), event, context }, null, 2),
   };
 };
